057688810a790ddeedbf80cde7e96080623343df
Nils Benz <nils.benz@adcubum.com>
2020-04-27 16:18:14 +0200

    feat: init pages

---
+++ src/components/pages/webdev/WebDev.tsx
@@ -0,0 +1,8 @@
+import { Typography } from '@material-ui/core';
+import React from 'react';
+
+const WebDev: React.FC = () => {
+  return <Typography variant="h1">Web Development</Typography>;
+};
+
+export default WebDev;
--- src/store/sagas.ts
+++
@@ -1,6 +0,0 @@
-import { SagaIterator } from "@redux-saga/types";
-import { all } from "redux-saga/effects";
-
-export default function* rootSagas(): SagaIterator {
-  yield all([]);
-}
